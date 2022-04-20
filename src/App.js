4576f3fb1093a52a76a8000a0dfec050d75447bf
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-04-20 16:56:44 -0400

    list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
 import "./styles.scss";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <List />
     </div>
   );
 }
