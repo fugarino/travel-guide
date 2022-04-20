2c8469ad7574a83327609295fc018a3662f26d4b
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-04-20 17:41:22 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
 import "./styles.scss";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <List />
+      <section className="layout">
+        <List />
+        <Map />
+      </section>
     </div>
   );
 }
