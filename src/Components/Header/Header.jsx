6ed65dce3735f92921cea8cc2421ab39a50d570d
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-04-21 14:49:05 -0400

    header styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <div className="header">
-      <h1>Header.js</h1>
+      <h2>Travel Guide</h2>
+      <div className="search-div">
+        <h3>Explore new places</h3>
+        {/* <Autocomplete> */}
+        <div className="search">
+          <FontAwesomeIcon icon={faSearch} className="icon" />
+          <input type="text" placeholder="Search..." />
+        </div>
+        {/* </Autocomplete> */}
+      </div>
     </div>
   );
 }
